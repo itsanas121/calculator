b1e7e5701b55609b5bdc8f2a1557e0d942a18b8a
Anas <anascs2023@gmail.com>
2024-06-22 03:26:57 +0300

    centred blue circle with red background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>آلة حاسبة</title>
+</svelte:head>
+<div class = "bg-red-500 h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center">
+    <div class = "bg-blue-500 rounded-full h-[5rem] w-[5rem]">
+
+    </div>
+</div>
